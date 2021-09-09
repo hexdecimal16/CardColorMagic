f29d2c3ae2acc5f328a4c51c280ecae5ce41e212
Dhairy Triapthi <hexdecimal16@gmail.com>
2021-09-09 11:43:06 +0530

    Separate initializer for views and listeners

--- app/src/main/java/com/dhairytripathi/cardcolormagic/ui/MainActivity.kt
+++ app/src/main/java/com/dhairytripathi/cardcolormagic/ui/MainActivity.kt
@@ -3,27 +3,36 @@ package com.dhairytripathi.cardcolormagic.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.dhairytripathi.cardcolormagic.R
 import com.dhairytripathi.cardcolormagic.adapter.CardColorAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_card.*
 
 private const val TAG = "MAIN_ACTIVITY"
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewModel: MainViewModel by viewModels()
         viewModel.onInit()
-        var cardColorAdapter: CardColorAdapter;
+        initListener()
+        initViews()
+    }
 
+    private fun initViews() {
+        tv_app_title.text = this.getText(R.string.app_name)
         rv_cards.apply {
             layoutManager = GridLayoutManager(context, 3)
             setHasFixedSize(true)
         }
-
+    }
+     
+    private fun initListener() {
+        var cardColorAdapter: CardColorAdapter
         fab_color_change_button.setOnClickListener { _ ->
             viewModel.switchState()
         }
